42d4065e4b21b127a6800f22f593bfde19b670b5
andela-uibrahim <usman.ibrahim@andela.com>
2021-05-16 21:00:38 -0400

    added post build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,6 +1,14 @@
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
     export SENTRY_PROPERTIES=./ios/sentry.properties
 
+    brew install jq
+
+    APP_VERSION=$(cat package.json | jq -r .version)
+    SENTRY_VERSION=""
+    BUNDLE_ID_IOS="org.reactjs.native.example.senntry"
+
+    SENTRY_VERSION="$BUNDLE_ID_IOS-$APP_VERSION"
+
     cp "$APPCENTER_SOURCE_DIRECTORY/index.ios.map" "$APPCENTER_SOURCE_DIRECTORY/dist/index.ios.map"
     cp "$APPCENTER_SOURCE_DIRECTORY/dummy-sourcemap-main.jsbundle" "$APPCENTER_SOURCE_DIRECTORY/dist/main.jsbundle"
 
