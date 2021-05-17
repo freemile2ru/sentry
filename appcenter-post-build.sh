26e5301870262abef4773e89be1123a2548182c7
andela-uibrahim <usman.ibrahim@andela.com>
2021-05-17 12:40:34 -0400

    removed pre build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,5 +1,9 @@
-if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
-    export SENTRY_PROPERTIES=./ios/sentry.properties
-    
-    ./node_modules/@sentry/cli/bin/sentry-cli releases files "0.0.1" upload-sourcemaps --dist "0.0.1" --rewrite ./main.jsbundle ./main.jsbundle.map
-fi
+
+brew install react-native-cli
+react-native bundle --dev false --platform ios --entry-file index.js --bundle-output ./main.jsbundle --sourcemap-output ./main.jsbundle.map
+
+export SENTRY_PROPERTIES=./ios/sentry.properties
+echo "Current branch is $APPCENTER_BRANCH"
+echo "Current branch is $APPCENTER_BRANCH"
+
+./node_modules/@sentry/cli/bin/sentry-cli releases files "0.0.1" upload-sourcemaps --dist "0.0.1" --rewrite ./main.jsbundle ./main.jsbundle.map
--- appcenter-pre-build.sh
+++
@@ -1,2 +0,0 @@
-brew install react-native-cli
-react-native bundle --dev false --platform ios --entry-file index.js --bundle-output ./main.jsbundle --sourcemap-output ./main.jsbundle.map
