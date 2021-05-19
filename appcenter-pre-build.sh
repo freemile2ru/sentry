a773f24c9d2988a065a1a79fc886c5238f80f51c
andela-uibrahim <usman.ibrahim@andela.com>
2021-05-19 14:47:52 -0400

    removed post build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,11 +1,6 @@
-brew install jq
-
-react-native bundle --dev false --platform ios --entry-file index.js --bundle-output ./main.jsbundle --sourcemap-output ./main.jsbundle.map
-react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./index.android.bundle --sourcemap-output ./index.android.bundle.map
-
-export SENTRY_PROPERTIES=./ios/sentry.properties
+git commit -m""brew install jq
 BUNDLE_ID_IOS="org.reactjs.native.example.senntry"
-APP_VERSION=$(cat package.json | jq -r .version)
+APP_VERSION=$(jq -r .version)
 SENTRY_VERSION="$BUNDLE_ID_IOS@${APP_VERSION}+$APPCENTER_BUILD_ID"
 
 echo “APPCENTER_BUILD_ID=$APPCENTER_BUILD_ID” >> .env
