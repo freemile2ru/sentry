e74d76d6402fd40409d90d4a8fd46c9cdc99a24b
andela-uibrahim <usman.ibrahim@andela.com>
2021-05-21 10:20:38 -0400

    fixed bash error

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -5,18 +5,18 @@ if [ -n "$APKFile" ]; then
   echo "We are in adroid ENV"
   export SENTRY_PROPERTIES=./ios/sentry.properties
   ENVIRONMENT=$APPCENTER_BRANCH
-  if [ "$APPCENTER_BRANCH"=="master" ]; then
+  if [ $APPCENTER_BRANCH = "master" ]; then
     ENVIRONMENT="production"  
   fi
   echo "========>$APPCENTER_BRANCH"
-  echo "========>"ENVIRONMENT
+  echo "========>$ENVIRONMENT"
   APP_VERSION="1.0"
   brew install react-native-cli
   BUNDLE_ID="org.reactjs.native.example.senntry"
   SENTRY_VERSION="$BUNDLE_ID@${APP_VERSION}+$APPCENTER_BUILD_ID"
   react-native bundle --dev false --platform android --entry-file index.js --bundle-output index.android.bundle --sourcemap-output index.android.bundle.map
   ./node_modules/@sentry/cli/bin/sentry-cli releases files $SENTRY_VERSION upload-sourcemaps --dist $APPCENTER_BUILD_ID --rewrite index.android.bundle index.android.bundle.map
-  ./node_modules/@sentry/cli/bin/sentry-cli  releases deploys new -e "$ENVIRONMENT"
+  ./node_modules/@sentry/cli/bin/sentry-cli  releases deploys new -e $ENVIRONMENT
 else
   echo "We are in IOS"
 fi
